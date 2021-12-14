f7a7a06762a99ccf110c60b931401b7536c649f2
戴佑軒 <smalltomo123@gmail.com>
2021-12-14 23:16:46 +0800

    LocalDB 連線建置
    修改AdoClass中 ReadData()中連線方法
    嘗試連線至LocalDb

--- ShaneShop/ShaneShop/Controllers/OrdersController.cs
+++ ShaneShop/ShaneShop/Controllers/OrdersController.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Data.SqlClient;
+using ShaneShop.Models.Util;
 
 namespace ShaneShop.Controllers
 {
@@ -18,7 +19,9 @@ namespace ShaneShop.Controllers
         public ActionResult CreateOrders()
         {
             AdoCommand adoCommand = new AdoCommand(SqlConnectString);
-            
+            string OrderID = "10418";
+            var testConnect = adoCommand.GetOrderByOrderID(OrderID);
+
             return View();
         }
     }
